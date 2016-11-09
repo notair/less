280183000e77199491404e2559b2cbbd9079bdf4
zQi-PC\zQi <318319903@qq.com>
2016-11-09 14:38:12 +0800

    add content-index

--- content-index.php
+++ content-index.php
@@ -43,20 +43,7 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ),
-				the_title( '<span class="screen-reader-text">', '</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-			) );	
-		?>
+		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
